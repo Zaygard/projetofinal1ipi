04446de80015adff27506c4071c4576432df1d99
zaygard <danielcmandrade@gmail.com>
2023-09-13 17:01:44 -0300

    inicio

---
+++ js.js
@@ -0,0 +1,27 @@
+function cadastro() {
+    while (true) {
+        l()
+    }
+    
+    function l() {
+        var lista_de_produtos = []
+        var opcao = prompt(`
+        bem vindo ao casdatro de produtos! :)
+        
+        1-cadastrar produto
+        2-remover produto
+        3-editar produto
+        4-ver produtos
+        `)
+        if (opcao == 1) {
+            /**codigo para adicionar produtos */
+            alert("ola mundo")
+        } else if (opcao == 2) {
+            /**codigo para remover produtos */
+        } else if (opcao == 3) {
+            /**codigo para editar produtos */
+        } else if (opcao == 4) {
+            /**codigo para ver os produtos */
+        }
+    }
+}
