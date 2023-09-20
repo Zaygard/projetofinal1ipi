4892b971b23be3adc359af2f28b12f3e1ce5de93
Zaygard <143035499+Zaygard@users.noreply.github.com>
2023-09-20 12:10:32 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,9 +1,8 @@
-function cadastro() {
+function projeto() {
     while (true) {
-        l()
-    }
-    
-    function l() {
+        
+        
+        
         var lista_de_produtos = []
         var opcao = prompt(`
         bem vindo ao casdatro de produtos! :)
@@ -15,13 +14,34 @@ function cadastro() {
         `)
         if (opcao == 1) {
             /**codigo para adicionar produtos */
-            alert("ola mundo")
+            cadastro()
         } else if (opcao == 2) {
             /**codigo para remover produtos */
         } else if (opcao == 3) {
             /**codigo para editar produtos */
         } else if (opcao == 4) {
             /**codigo para ver os produtos */
+            olhar_produtos()
         }
     }
-}
+        
+        function cadastro() {
+            var adicionar = {
+                nome: "",
+                marca: "",
+                preco: ""
+            }
+            adicionar.nome = prompt("nome");
+            adicionar.marca = prompt("marca")
+            adicionar.preço = prompt("preco")
+            lista_de_produtos.push(adicionar)
+        }
+
+        function olhar_produtos() {
+            var texto =""
+            for (let i = 0; i < lista.length; i++) {
+                texto+= `nome:${lista_de_produtos[i].nome} marca:${lista_de_produtos[i].marca} preço:${lista_de_produtos[i].preco}\n`
+            }
+            alert(texto)
+        }
+    }
